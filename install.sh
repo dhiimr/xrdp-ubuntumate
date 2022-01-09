4b18b87e5d83d249d7eb2b1a0d2a9500136d1545
Edi Susanto <51095316+dhiimr@users.noreply.github.com>
2022-01-09 12:13:49 +0700

    Update install.sh
    Ok

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
+sudo apt-get install xrdp -y
 sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon -y
 sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
 sudo /etc/init.d/xrdp restart
