9340f4d349061f16751108aa94ca70feedbe7a91
Edi Susanto <51095316+dhiimr@users.noreply.github.com>
2022-04-03 23:23:18 +0700

    Update install.sh
    Gdm

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install xrdp -y
-sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon -y
+sudo apt-get install gdm3 mate-core mate-desktop-environment mate-notification-daemon -y
 sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
 sudo /etc/init.d/xrdp restart
 sudo apt install tigervnc-xorg-extension tigervnc-standalone-server tigervnc-common -y
